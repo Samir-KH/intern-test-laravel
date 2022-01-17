37846ed76a5fe1bc7fd4f5e8387c9f9df180b8ff
Gitlab <samir.elkhyati@gmail.com>
2022-01-17 23:18:03 +0100

    css colorize the chart

--- resources/views/charts.blade.php
+++ resources/views/charts.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href= "{{ secure_asset('css/chart.css') }}">
+    <link rel="stylesheet" href= "{{ asset('css/chart.css') }}">
     <title>Document</title>
 </head>
 <body>
